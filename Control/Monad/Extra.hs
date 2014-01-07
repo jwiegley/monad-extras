f4edde001ba6a2eff3a9d7d84f1a4a9b7ff498af
John Wiegley <johnw@newartisans.com>
2014-01-06 21:16:55 -0600

    Remove ComposeT, since it's now a part of mmorph

--- Control/Monad/Extra.hs
+++ Control/Monad/Extra.hs
@@ -213,26 +213,3 @@ sequenceWhile p (m:ms) = do
             as <- sequenceWhile p ms
             return (a:as)
         else return []
-
--- | A type wrapper for composing monad transformers.  This is very similar to
---   'Data.Functor.Compose', just one level up.
-newtype ComposeT (f :: (* -> *) -> * -> *) (g :: (* -> *) -> * -> *) m a
-    = ComposeT { getComposeT :: f (g m) a }
-    deriving (Functor, Applicative, Monad, MonadIO)
-
-instance (MFunctor f, MonadTrans f, MonadTrans g)
-         => MonadTrans (ComposeT f g) where
-    lift = ComposeT . hoist lift . lift
-
-instance (MonadIO (f (g m)), Applicative (f (g m)))
-         => MonadBase IO (ComposeT f g m) where
-    liftBase = liftIO
-
-instance (Applicative (f (g m)), MonadBaseControl IO (f (g m)),
-          MonadIO (f (g m)))
-         => MonadBaseControl IO (ComposeT f g m) where
-    newtype StM (ComposeT f g m) a = StMComposeT (StM (f (g m)) a)
-    liftBaseWith f =
-        ComposeT $ liftBaseWith $ \runInBase -> f $ \k ->
-            liftM StMComposeT $ runInBase $ getComposeT k
-    restoreM (StMComposeT m) = ComposeT . restoreM $ m
